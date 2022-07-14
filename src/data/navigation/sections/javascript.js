9564dffab2ecd97a43ce6e7bd2c95c3b2ebac96e
Jeff Matthews <matthews.jeffery@gmail.com>
2022-07-14 10:30:50 -0500

    Created TOC

--- src/data/navigation/sections/javascript.js
+++ src/data/navigation/sections/javascript.js
@@ -1,6 +1,184 @@
 module.exports = [
     {
-      title: "Introduction",
+      title: "Overview",
       path: "/javascript/",
+      header: true,
+      pages: [
+        {
+          title: "Introduction",
+          path: "/javascript/",
+        },
+        {
+          title: "Conventional notations",
+          path: "/javascript/conventions/",
+        },
+        {
+          title: "JS resources in Commerce",
+          path: "/javascript/resources/",
+        },
+        {
+          title: "RequireJS",
+          path: "/javascript/requirejs/",
+        },
+        {
+          title: "Call and initialize JS",
+          path: "/javascript/init/",
+        },
+        {
+          title: "Frontend product repository",
+          path: "/javascript/frontend-product-repository/",
+        },
+        {
+          title: "Custom JS",
+          path: "/javascript/custom/",
+        },
+        {
+          title: "JS customization tutorial",
+          path: "/javascript/conventions/",
+        },
+        {
+          title: "Locate JS components",
+          path: "/javascript/debug/",
+        },
+        {
+          title: "Mixins",
+          path: "/javascript/mixins/",
+        },
+        {
+          title: "Logger",
+          path: "/javascript/logger/",
+        },
+      ],
+    },
+    {
+      title: "jQuery widgets",
+      path: "/javascript/jquery-widgets/",
+      header: true,
+      pages: [
+        {
+          title: "Introduction",
+          path: "/javascript/jquery-widgets/",
+        },
+        {
+          title: "Accordion",
+          path: "/javascript/jquery-widgets/accordion/",
+        },
+        {
+          title: "Alert",
+          path: "/javascript/jquery-widgets/alert/",
+        },
+        {
+          title: "Breadcrumbs",
+          path: "/javascript/jquery-widgets/breadcrumbs/",
+        },
+        {
+          title: "Calendar",
+          path: "/javascript/jquery-widgets/calendar/",
+        },
+        {
+          title: "Collapsible",
+          path: "/javascript/jquery-widgets/collapsible/",
+        },
+        {
+          title: "Confirmation",
+          path: "/javascript/jquery-widgets/confirm/",
+        },
+        {
+          title: "Dropdown",
+          path: "/javascript/jquery-widgets/dropdown/",
+        },
+        {
+          title: "Dropdown dialog",
+          path: "/javascript/jquery-widgets/dialog/",
+        },
+        {
+          title: "FolderTree",
+          path: "/javascript/jquery-widgets/folder-tree/",
+        },
+        {
+          title: "Gallery",
+          path: "/javascript/jquery-widgets/gallery/",
+        },
+        {
+          title: "List",
+          path: "/javascript/jquery-widgets/list/",
+        },
+        {
+          title: "Loader",
+          path: "/javascript/jquery-widgets/loader/",
+        },
+        {
+          title: "Magnifier",
+          path: "/javascript/jquery-widgets/magnifier/",
+        },
+        {
+          title: "MediaUploader",
+          path: "/javascript/jquery-widgets/media-uploader/",
+        },
+        {
+          title: "Menu",
+          path: "/javascript/jquery-widgets/menu/",
+        },
+        {
+          title: "Modal",
+          path: "/javascript/jquery-widgets/modal/",
+        },
+        {
+          title: "Multiselect",
+          path: "/javascript/jquery-widgets/multiselect/",
+        },
+        {
+          title: "Navigation",
+          path: "/javascript/jquery-widgets/navigation/",
+        },
+        {
+          title: "PasswordStrengthIndicator",
+          path: "/javascript/jquery-widgets/password-strength-indicator/",
+        },
+        {
+          title: "PopupWindow",
+          path: "/javascript/jquery-widgets/popup-window/",
+        },
+        {
+          title: "Prompt",
+          path: "/javascript/jquery-widgets/prompt/",
+        },
+        {
+          title: "QuickSearch",
+          path: "/javascript/jquery-widgets/quick-search/",
+        },
+        {
+          title: "RedirectUrl",
+          path: "/javascript/jquery-widgets/redirect-url/",
+        },
+        {
+          title: "RemainingCharacters",
+          path: "/javascript/jquery-widgets/remaining-characters/",
+        },
+        {
+          title: "RowBuilder",
+          path: "/javascript/jquery-widgets/row-builder/",
+        },
+        {
+          title: "Sortable",
+          path: "/javascript/jquery-widgets/sortable/",
+        },
+        {
+          title: "Sticky",
+          path: "/javascript/jquery-widgets/sticky/",
+        },
+        {
+          title: "Tabs",
+          path: "/javascript/jquery-widgets/tabs/",
+        },
+        {
+          title: "ToggleAdvanced",
+          path: "/javascript/jquery-widgets/toggle/",
+        },
+        {
+          title: "TrimInput",
+          path: "/javascript/jquery-widgets/trim-input/",
+        },
+      ],
     },
   ];
