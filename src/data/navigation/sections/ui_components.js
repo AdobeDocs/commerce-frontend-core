068dd3d56ba533d62d4607fea657e9776786fd39
Jeff Matthews <matthews.jeffery@gmail.com>
2022-07-19 16:06:07 -0500

    Created TOC and added images

--- src/data/navigation/sections/ui_components.js
+++ src/data/navigation/sections/ui_components.js
@@ -6,17 +6,389 @@ module.exports = [
     {
       title: "Concepts",
       path: "/ui-components/concepts/",
+      pages: [
+        {
+          title: "Configuration flow",
+          path: "/ui-components/concepts/configuration-flow/",
+        },
+        {
+          title: "XML configuration",
+          path: "/ui-components/concepts/xml-configuration/",
+        },
+        {
+          title: "Data sourcing",
+          path: "/ui-components/concepts/data-source/",
+        },
+        {
+          title: "PHP modifiers",
+          path: "/ui-components/concepts/modifier/",
+        },
+        {
+          title: "Template literals",
+          path: "/ui-components/concepts/template-literals/",
+        },
+        {
+          title: "Binding syntax",
+          path: "/ui-components/concepts/binding-syntax/",
+        },
+        {
+          title: "Custom Knockout.js bindings",
+          path: "/ui-components/concepts/knockout-bindings/",
+        },
+        {
+          title: "uiClass library",
+          path: "/ui-components/concepts/class/",
+        },
+        {
+          title: "uiElement class",
+          path: "/ui-components/concepts/element/",
+        },
+        {
+          title: "uiCollection class",
+          path: "/ui-components/concepts/collection/",
+        },
+        {
+          title: "uiLayout service object",
+          path: "/ui-components/concepts/layout/",
+        },
+        {
+          title: "Property linking",
+          path: "/ui-components/concepts/linking/",
+        },
+        {
+          title: "uiRegistry library",
+          path: "/ui-components/concepts/registry/",
+        },
+      ],
     },
     {
       title: "Components",
-      path: "/ui-components/components/",
+      path: "/ui-components/",
+      pages: [
+        {
+          title: "ActionDelete",
+          path: "/ui-components/action-delete/"
+        },
+        {
+          title: "ActionsColumn",
+          path: "/ui-components/actions-column/"
+        },
+        {
+          title: "Bookmarks",
+          path: "/ui-components/bookmarks/"
+        },
+        {
+          title: "Button",
+          path: "/ui-components/button/"
+        },
+        {
+          title: "Checkbox",
+          path: "/ui-components/checkbox/"
+        },
+        {
+          title: "CheckboxToggleNotice",
+          path: "/ui-components/checkbox-toggle-notice/"
+        },
+        {
+          title: "Checkboxset",
+          path: "/ui-components/checkbox-set/"
+        },
+        {
+          title: "ColorPicker",
+          path: "/ui-components/color-picker/"
+        },
+        {
+          title: "Column",
+          path: "/ui-components/column/"
+        },
+        {
+          title: "Columns",
+          path: "/ui-components/columns/"
+        },
+        {
+          title: "ColumnsEditor",
+          path: "/ui-components/columns-editor/"
+        },
+        {
+          title: "ColumnsEditorRecord",
+          path: "/ui-components/columns-editor-record/"
+        },
+        {
+          title: "ColumnsEditorView",
+          path: "/ui-components/columns-editor-view/"
+        },
+        {
+          title: "ColumnsEditingBulk",
+          path: "/ui-components/columns-editing-bulk/"
+        },
+        {
+          title: "ColumnsEDitingClient",
+          path: "/ui-components/columns-editing-client/"
+        },
+        {
+          title: "ColumnsResize",
+          path: "/ui-components/columns-resize/"
+        },
+        {
+          title: "ColumnsControls",
+          path: "/ui-components/columns-controls/"
+        },
+        {
+          title: "Container",
+          path: "/ui-components/container/"
+        },
+        {
+          title: "Date",
+          path: "/ui-components/date/"
+        },
+        {
+          title: "DateColumn",
+          path: "/ui-components/date-column/"
+        },
+        {
+          title: "DragAndDrop",
+          path: "/ui-components/drag-and-drop/"
+        },
+        {
+          title: "DynamicRows",
+          path: "/ui-components/dynamic-rows/"
+        },
+        {
+          title: "DynamicRowsDragAndDrop",
+          path: "/ui-components/dynamic-rows-drag-drop/"
+        },
+        {
+          title: "DynamicRowsRecord",
+          path: "/ui-components/dynamic-rows-record/"
+        },
+        {
+          title: "Email",
+          path: "/ui-components/email/"
+        },
+        {
+          title: "ExpandableColumn",
+          path: "/ui-components/expandable-column/"
+        },
+        {
+          title: "ExportButton",
+          path: "/ui-components/export-button/"
+        },
+        {
+          title: "Fieldset",
+          path: "/ui-components/fieldset/"
+        },
+        {
+          title: "File",
+          path: "/ui-components/file/"
+        },
+        {
+          title: "FileUploader",
+          path: "/ui-components/file-uploader/"
+        },
+        {
+          title: "Filters",
+          path: "/ui-components/filters/"
+        },
+        {
+          title: "FiltersChips",
+          path: "/ui-components/filters-chips/"
+        },
+        {
+          title: "Form",
+          path: "/ui-components/form/"
+        },
+        {
+          title: "FormDataProvider",
+          path: "/ui-components/form-data-provider/"
+        },
+        {
+          title: "GridDataProvider",
+          path: "/ui-components/grid-data-provider/",
+        },
+        {
+          title: "Hidden",
+          path: "/ui-components/hidden/",
+        },
+        {
+          title: "HtmlContent",
+          path: "/ui-components/html-content/",
+        },
+        {
+          title: "ImagePreview",
+          path: "/ui-components/image-preview/",
+        },
+        {
+          title: "ImageUploader",
+          path: "/ui-components/image-uploader/",
+        },
+        {
+          title: "Input",
+          path: "/ui-components/input/",
+        },
+        {
+          title: "InsertForm",
+          path: "/ui-components/insert-form/",
+        },
+        {
+          title: "InsertListing",
+          path: "/ui-components/insert-listing/",
+        },
+        {
+          title: "LinkColumn",
+          path: "/ui-components/link-column/",
+        },
+        {
+          title: "Listing (grid)",
+          path: "/ui-components/listing-grid/",
+        },
+        {
+          title: "Masonry (grid)",
+          path: "/ui-components/masonry/",
+        },
+        {
+          title: "MassActions",
+          path: "/ui-components/mass-actions/",
+        },
+        {
+          title: "Modal",
+          path: "/ui-components/modal/",
+        },
+        {
+          title: "Multiline",
+          path: "/ui-components/multiline/",
+        },
+        {
+          title: "Multiselect",
+          path: "/ui-components/multiselect/",
+        },
+        {
+          title: "MultiselectColumn",
+          path: "/ui-components/multiselect-column/",
+        },
+        {
+          title: "Navigation",
+          path: "/ui-components/navigation/",
+        },
+        {
+          title: "OnOffColumn",
+          path: "/ui-components/on-off-column/",
+        },
+        {
+          title: "Paging",
+          path: "/ui-components/paging/",
+        },
+        {
+          title: "Radioset",
+          path: "/ui-components/radio-set/",
+        },
+        {
+          title: "Range",
+          path: "/ui-components/range/",
+        },
+        {
+          title: "Search",
+          path: "/ui-components/search/",
+        },
+        {
+          title: "Select",
+          path: "/ui-components/select/",
+        },
+        {
+          title: "SelectColumn",
+          path: "/ui-components/select-column/",
+        },
+        {
+          title: "Sizes",
+          path: "/ui-components/sizes/",
+        },
+        {
+          title: "SortBy",
+          path: "/ui-components/sortby/",
+        },
+        {
+          title: "Tab",
+          path: "/ui-components/tab/",
+        },
+        {
+          title: "Text",
+          path: "/ui-components/text/",
+        },
+        {
+          title: "Textarea",
+          path: "/ui-components/text-area/",
+        },
+        {
+          title: "ThumbnailColumn",
+          path: "/ui-components/thumbnail-column/",
+        },
+        {
+          title: "TimelineColumns",
+          path: "/ui-components/timeline-columns/",
+        },
+        {
+          title: "ListingToolbar",
+          path: "/ui-components/toolbar/",
+        },
+        {
+          title: "TreeMassActions",
+          path: "/ui-components/tree-mass-actions/",
+        },
+        {
+          title: "UrlInput",
+          path: "/ui-components/url-input/",
+        },
+        {
+          title: "UI-select",
+          path: "/ui-components/select/",
+        },
+        {
+          title: "WYSIWYG",
+          path: "/ui-components/wysiwyg/",
+          pages: [
+            {
+              title: "Add custom editor",
+              path: "/ui-components/wysiwyg/add-custom-editor/",
+            },
+            {
+              title: "Configure TinyMCE editor",
+              path: "/ui-components/wysiwyg/configure-tinymce-editor/",
+            },
+            {
+              title: "Extension points",
+              path: "/ui-components/wysiwyg/extension-points/",
+            },
+          ],
+        },
+      ],
     },
     {
       title: "How to",
       path: "/ui-components/howto/",
+      pages: [
+        {
+          title: "Create and display a category attribute",
+          path: "/ui-components/howto/add-category-attribute/",
+        },
+        {
+          title: "Update the page URL type",
+          path: "/ui-components/howto/update-url-type/",
+        },
+        {
+          title: "Render prices on the frontend",
+          path: "/ui-components/howto/price-rendering/",
+        },
+        {
+          title: "Declare a custom component",
+          path: "/ui-components/howto/new-component-declaration/",
+        },
+        {
+          title: "Add a show/hide password checkbox",
+          path: "/ui-components/howto/show-hide-password-checkbox/",
+        },
+      ],
     },
     {
-      title: "Troubleshoot",
-      path: "/ui-components/troubleshoot/",
+      title: "Debugging",
+      path: "/ui-components/debug/",
     },
   ];
